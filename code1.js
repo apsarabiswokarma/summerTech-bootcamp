5b34cccd86abca9e9dc88b6d170922925fda9747
Apsara Bishwokarma <apsarabishwokarma7@gmail.com>
2022-06-30 09:24:02 +0545

    js learning

---
+++ code1.js
@@ -0,0 +1,52 @@
+function test(a,b){
+    return a+b;
+  }
+  console.log("the result is",test( 2,5));
+
+
+  parseInt(34.7770)
+
+  let a=8;
+console.log(`the value of a is ${a}`);
+
+function value()
+    {
+        return"js";}
+        console.log(`this is class of ${value()}`);
+
+        for(i=0;i<10;i++)
+    {
+        console.log(i)
+    }
+    const array=["apple","mango","banana","strawberry"];
+for(let i=0;i<array.length;i++){
+    console.log(array[i]);
+}
+let b ={name:'anjila'}
+b.name
+console.log(b.name);
+function check(a){
+    if(a%2==0)
+    {
+        console.log("it is even number");
+
+    }
+    else{
+        console.log("it is odd number");
+
+    }
+}
+check(30);
+
+function spare(d,e,...f)
+{
+    if(d==e)
+    {
+        console.log("true");
+    }
+    else{
+        console.log("false");
+    }
+    console.log(f)
+}
+spare(3,6,4,8,9,6);
